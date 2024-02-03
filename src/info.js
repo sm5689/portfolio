9387ed51b240e7020661659c5362b2d74ec888d4
Suraj Mishra <72648167+sm5689@users.noreply.github.com>
2024-02-03 11:03:04 -0500

    Initial commit

---
+++ src/info.js
@@ -0,0 +1,122 @@
+export const i = {
+    firstName: "Jensen",
+    secondName: "Lakes",
+    occupation: "Digital Director",
+    description: "Hi, I'm Jensen. I have over 10 years of experience as a digital director with a multidisciplinary background. My main goal is to create meaningful products that cause positive disruption.",
+    status: "⚡︎ Open for remote/async offers",
+    email: "📧 jensen@jensenlakes.com",
+    photo: "../src/assets/pic.png",
+    featuredWork: [
+        {
+            title: "Company X",
+            dates: "2010 — 2015"
+        },
+        {
+            title: "Company Y",
+            dates: "2015 — 2020"
+        },
+        {
+            title: "Company Z",
+            dates: "2020 — now"
+        },
+    ],
+    recentProjects: [
+        {
+            title: "Project X",
+            url: "0xshapeshifter.dev"
+        },
+        {
+            title: "Project y",
+            url: "0xshapeshifter.dev"
+        },
+        {
+            title: "Project Z",
+            url: "0xshapeshifter.dev"
+        },
+    ],
+    recognition: [
+        {
+            title: "Award X",
+            date: "2010"
+        },
+        {
+            title: "Award Y",
+            date: "2015"
+        },
+        {
+            title: "Award Z",
+            date: "2020"
+        },
+    ],
+    socialMedia: [
+        {
+            title: "X",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/x.svg",
+                light: "../src/assets/social/light/x.svg",
+            }
+        },
+        {
+            title: "Mastodon",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/mt.svg",
+                light: "../src/assets/social/light/mt.svg",
+            }
+        },
+        {
+            title: "Telegram",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/tg.svg",
+                light: "../src/assets/social/light/tg.svg",
+            }
+        },
+        {
+            title: "Facebook",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/fb.svg",
+                light: "../src/assets/social/light/fb.svg",
+            }
+        },
+        {
+            title: "Instagram",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/ig.svg",
+                light: "../src/assets/social/light/ig.svg",
+            }
+        },
+        {
+            title: "LinkedIn",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/lin.svg",
+                light: "../src/assets/social/light/lin.svg",
+            }
+        },
+        {
+            title: "Youtube",
+            url: "https://google.com",
+            alt: "X logo",
+            images: {
+                dark: "../src/assets/social/dark/yt.svg",
+                light: "../src/assets/social/light/yt.svg",
+            }
+        },
+    ],
+    // Thank you for leaving this part and supporting my work
+    support: {
+        sign: "＊",
+        title: "by 0xhapeshifter",
+        url: "https://0xshapeshifter.dev/productize"
+    }
+}
